fb3879dc41007f060754d7131af35a54dea97447
wacoss <52801670+wacoss@users.noreply.github.com>
2025-11-17 17:45:04 -0300

    Create testgolive.php

---
+++ testgolive.php
@@ -0,0 +1,7 @@
+
+<link rel="stylesheet" type="text/css" href="https://www.golive.cl/canal/css/styles.css?v=3.0">
+
+<iframe src="https://www.youtube.com/embed/QLjC0rcO5IA?autoplay=1" frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen></iframe>
